53c0a2d9c65fc2882e90bb7004b57cc88808ae2c
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2023-02-02 15:39:12 +0500

    Connect books component to Redux store

--- src/components/pages/Books.js
+++ src/components/pages/Books.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import BookItem from '../BookItem';
 import AddBook from '../AddBook';
 
-const Books = () => (
-  <div>
-    <BookItem title="Book-1" author="Author-1" />
-    <BookItem title="Book-2" author="Author-2" />
+const Books = () => {
+  const books = useSelector((state) => state.books);
 
-    <AddBook />
-  </div>
-);
+  return (
+    <section>
+      <ul>
+        {books.map((book) => (
+          <li key={book.id}>
+            <BookItem book={book} />
+          </li>
+        ))}
+      </ul>
+      <AddBook />
+    </section>
+  );
+};
 
 export default Books;
