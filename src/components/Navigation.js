020c85d36b261404697e5dc1c9a97aff47b00cc4
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2023-02-05 00:06:48 +0500

    Apply styling for navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import avatar from './account.svg';
 
 const Navigation = () => (
-  <nav>
-    <Link to="/">Home</Link>
-    <Link to="/categories">
-      Categories
-    </Link>
+  <nav className="nav-bar">
+    <Link className="nav-title" to="/">Bookstore CMS</Link>
+    <ul className="nav-links">
+      <li>
+        <NavLink className="nav-link" activeClassName="active" to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink className="nav-link" activeClassName="active" to="/categories">Categories</NavLink>
+      </li>
+    </ul>
+    <button className="avatar-btn" type="button">
+      <img src={avatar} className="avatar" alt="Account avatar" />
+    </button>
   </nav>
 );
 
